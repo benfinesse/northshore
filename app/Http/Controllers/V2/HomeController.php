15b54dfd8f8d45bf02e8b3343cd57d352b674aa1
Benjamin Isong <beisongabc@gmail.com>
2021-03-19 15:06:15 +0100

    small fix

--- app/Http/Controllers/V2/HomeController.php
+++ app/Http/Controllers/V2/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
     public function index(){
         $banner = Content::inRandomOrder()->where('type','banner')->where('group', 'home')->first();
-        $banner2 = Content::inRandomOrder()->where('type','banner')->where('group', 'home')->where('id', '!=', $banner->id)->first();
+        $banner2 = Content::inRandomOrder()->where('type','banner')->where('group', 'home')->where('id', '!=', @$banner->id)->first();
         $products = Product::where('active', true)->inRandomOrder()->take(6)->get();
         $randomProduct = Product::where('active', true)->inRandomOrder()->first();
         return view('pages.v2.home.index')
