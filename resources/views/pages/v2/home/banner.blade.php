c828108d1445727f9152819911b187241d7a678e
Benjamin Isong <beisongabc@gmail.com>
2021-03-31 18:21:01 +0100

    small fix

--- resources/views/pages/v2/home/banner.blade.php
+++ resources/views/pages/v2/home/banner.blade.php
@@ -1,16 +1,19 @@
 <section class="banner-section" style="background: url('{{ $banner->logo1() }}');">
     <div class="">
         <div class="banner-inner">
-            <div class="banner-content" style="background: rgba(45,45,45,0.68); width: 100%; position: absolute;bottom: 0;">
-                <div class="container m-3">
-                    <h2 class="page-title text-center">
-                        {{ $banner->title }}
-                        @if(!empty($banner->btn))
-                            <a href="{{ $banner->target }}" class="theme-btn style-two">{{ $banner->btn }}</a>
-                        @endif
-                    </h2>
+            @if(!empty($banner->title))
+                <div class="banner-content" style="background: rgba(45,45,45,0.68); width: 100%; position: absolute;bottom: 0;">
+                    <div class="container m-3">
+                        <h2 class="page-title text-center">
+                            {{ $banner->title }}
+                            @if(!empty($banner->btn))
+                                <a href="{{ $banner->target }}" class="theme-btn style-two">{{ $banner->btn }}</a>
+                            @endif
+                        </h2>
+                    </div>
                 </div>
-            </div>
+            @endif
+
         </div>
     </div>
     <div class="banner-angle">
