0e9639aa2082dae7b2245d6769ad19bdbf473e3f
Benjamin Isong <beisongabc@gmail.com>
2021-03-31 15:42:43 +0100

    small fix

--- resources/views/pages/v2/home/index.blade.php
+++ resources/views/pages/v2/home/index.blade.php
@@ -3,6 +3,21 @@ $navlink['home'] = 'current';
 ?>
 @extends('layouts.v2.app')
 
+@section('custom_css')
+    <style>
+        .team-item {
+            margin-bottom: 30px;
+            align-content: center;
+            display: grid;
+            text-align: -webkit-center;
+        }
+        .owl-carousel .owl-item img {
+            height: 250px !important;
+            width: auto !important;
+        }
+    </style>
+@endsection
+
 
 @section('content')
 
